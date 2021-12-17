d8aef1be160ec14c89af0b2b972f24cf76c27e1c
Ahmet Fatih <afyilmaz71@gmail.com>
2021-12-17 23:37:26 +0300

    changed background color

--- ViewController.swift
+++ ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print("make some changes")
+        print("changed background color")
         
 
     }
