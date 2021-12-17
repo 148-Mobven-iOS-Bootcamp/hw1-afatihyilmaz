dc6005ab666eb17f9da0cffdd3778193089d89ac
Ahmet Fatih <afyilmaz71@gmail.com>
2021-12-17 23:30:10 +0300

    added some features

--- ViewController.swift
+++ ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print("created feature D")
+        print("add some features")
         
 
     }
